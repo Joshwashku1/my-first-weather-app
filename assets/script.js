2d4ae26848ad7571c2d0f66c234b0ba64784cfef
JoshuaHamann <joshwashku@yahoo.com>
2022-12-02 11:39:47 -0600

    added and event listener to search button and a url to search a city

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+var dataE = document.querySelector('.input-data');
+var getE = document.getElementById('search-b');
+var getInput = document.getElementById('search-input');
+
+var APIKey = "e061528d54d5657e594e68a2750d11be";
+
+
+console.log(getE);
+console.log(getInput);
+console.log(dataE);
+
+ function searchAPI(city){
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +
+ "&appid=" + APIKey;
+
+    fetch(queryURL)
+    .then(function (response) {
+        console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var listItem = document.createElement('li');
+        listItem.textContent = data.city.name;
+        dataE.appendChild(listItem);
+    //   for (var i = 0; i < data.length; i++) {
+    //     var listItem = document.createElement('li');
+    //     listItem.textContent = data[i].city.name;
+    //     console.log("This is: " + listItem);
+    //     dataE.appendChild(listItem);
+    //   }
+    });
+}
+
+function handleSearchBtn(event){
+    event.preventDefault();
+
+    var userInput = getInput.value;
+
+    if(userInput){
+        searchAPI(userInput);
+    }
+
+}
+
+// searchAPI();
+function thisSays(){
+    console.log('hello');
+}
+
+getE.addEventListener('click', handleSearchBtn);
+
+
+
