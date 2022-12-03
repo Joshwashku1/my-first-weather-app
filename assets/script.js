3843af464a20b0104979d258c124d45afe8b77dd
JoshuaHamann <joshwashku@yahoo.com>
2022-12-03 09:23:18 -0600

    added kelvin converter for temperature

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,9 @@
-var dataE = document.querySelector('.input-data');
 var getE = document.getElementById('search-b');
 var getInput = document.getElementById('search-input');
 
 var APIKey = "e061528d54d5657e594e68a2750d11be";
 
 
-console.log(getE);
-console.log(getInput);
-console.log(dataE);
 
  function searchAPI(city){
 
@@ -23,7 +19,7 @@ console.log(dataE);
         console.log(data);
         var listItem = document.createElement('li');
         listItem.textContent = data.city.name;
-        dataE.appendChild(listItem);
+        body.appendChild(listItem);
     //   for (var i = 0; i < data.length; i++) {
     //     var listItem = document.createElement('li');
     //     listItem.textContent = data[i].city.name;
@@ -44,11 +40,14 @@ function handleSearchBtn(event){
 
 }
 
-// searchAPI();
-function thisSays(){
-    console.log('hello');
+function kelvinToFar(kelvin){
+    var fahrenheit = kelvin-273;
+    fahrenheit = fahrenheit*1.8;
+    fahrenheit = fahrenheit + 32;
+    return fahrenheit;
 }
 
+console.log(kelvinToFar(288));
 getE.addEventListener('click', handleSearchBtn);
 
 
