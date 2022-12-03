6e98b8268f83baa32e5d8e3eadfccb857dac5559
JoshuaHamann <joshwashku@yahoo.com>
2022-12-03 14:05:26 -0600

    added some css and changed the API to contain fahrenheit

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var getE = document.getElementById('search-b');
 var getInput = document.getElementById('search-input');
+var resultC = document.getElementById('current-day');
 
 var APIKey = "e061528d54d5657e594e68a2750d11be";
 
@@ -8,7 +9,7 @@ var APIKey = "e061528d54d5657e594e68a2750d11be";
  function searchAPI(city){
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +
- "&appid=" + APIKey;
+ "&appid=" + APIKey +"&units=imperial";
 
     fetch(queryURL)
     .then(function (response) {
@@ -17,9 +18,7 @@ var APIKey = "e061528d54d5657e594e68a2750d11be";
     })
     .then(function (data) {
         console.log(data);
-        var listItem = document.createElement('li');
-        listItem.textContent = data.city.name;
-        body.appendChild(listItem);
+        displayCurrent(data);
     //   for (var i = 0; i < data.length; i++) {
     //     var listItem = document.createElement('li');
     //     listItem.textContent = data[i].city.name;
@@ -40,12 +39,21 @@ function handleSearchBtn(event){
 
 }
 
-function kelvinToFar(kelvin){
-    var fahrenheit = kelvin-273;
-    fahrenheit = fahrenheit*1.8;
-    fahrenheit = fahrenheit + 32;
-    return fahrenheit;
+function displayCurrent(data){
+    var cityName = data.city.name;
+    var temp = Math.floor(data.list[0].main.temp) + "F";
+    var wind = data.list[0].wind.speed + " MPH";
+    var humidity = data.list[0].main.humidity + "%";
+
+    document.getElementById('current-city').textContent = cityName;
+    document.getElementById('current-1').textContent = "Temp: " + temp;
+    document.getElementById('current-2').textContent = "Wind: " + wind;
+    document.getElementById('current-3').textContent = "Humidity: " + humidity;
+    
+
 }
+
+
 
 console.log(kelvinToFar(288));
 getE.addEventListener('click', handleSearchBtn);
